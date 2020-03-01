66e2a0009115f16db6f92ff4ba77287c305af476
yudachi <371722931@qq.com>
2020-03-01 10:04:15 +0800

    commit demo0.0.1

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping(path = "/hello")
     public String hello() {
-        return "hello world";
+        return "hello world 0.0.1";
     }
 }
